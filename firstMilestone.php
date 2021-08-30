3d7b0fe4697b58af6cac527fd0f6cfc2af198b6e
adinapop <adinampop@yahoo.com>
2021-08-30 14:37:28 +0200

    First Milestone done

--- firstMilestone.php
+++ firstMilestone.php
@@ -1,6 +1,8 @@
 <!-- Prima Milestone:
 Stampiamo i dischi solo con l’utilizzo di PHP, 
-che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
+che stampa direttamente i dischi in pagina: 
+al caricamento della pagina ci saranno tutti i dischi. 
+-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,60 +11,46 @@ che stampa direttamente i dischi in pagina: al caricamento della pagina ci saran
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <title>First Milestone</title>
 </head>
 <body>
 
+    <?php 
+        // I require the array from the database
+        require_once __DIR__ . "/database/discsDb.php";
+    ?>
+
     <nav>
         <div class="logo-spotify">
-            <img src="./img/spotify-logo.png" alt="Spotify Logo">
+            <img class="wd" src="./img/spotify-logo.png" alt="Spotify Logo">
         </div>
     </nav>
 
     <main>
+        <section class="discs-container">
 
-        <?php
-
-            // I require the array from the database
-            require_once __DIR__ . "/database/discsDb.php";
-
-            // with foreach cycle all the array and print it
-            foreach($discs as $disc) {
-
-        ?>
-
-        <div>
-            <div>
-                <img src="<?php echo "{$disc['poster']}";?>" alt="">
+            <?php
+                // with foreach cycle all the array and print it
+                foreach($discs as $disc) {
+            ?>
+            <div class="album-container">
+                
+                <div class="inner">
+                    <img class="wd" src="<?php echo "{$disc['poster']}";?>" alt="<?php echo "{$disc['title']} Poster";?>">
+                    <h2><?php echo "{$disc['title']}";?></h2>
+                    <p><?php echo "{$disc['author']}";?></p>
+                    <p><?php echo "{$disc['year']}";?></p>
+                </div>
+    
             </div>
+            <?php }; ?>
 
-            <h2>
-                <?php
-                    echo "{$disc['title']}";
-                ?>
-            </h2>
-
-            <p>
-            <?php
-                    echo "{$disc['author']}";
-                ?>
-            </p>
-
-            <p>
-            <?php
-                    echo "{$disc['year']}";
-                ?>
-            </p>
-
-        </div>
-
-
-        <?php } ?>
-
-        ?>
+        </section>
 
     </main>
 
-    
 </body>
 </html>
