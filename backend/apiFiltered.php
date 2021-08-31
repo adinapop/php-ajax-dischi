0fd0ea440d1b677c04c998804d4fa39db49fa8f4
adinapop <adinampop@yahoo.com>
2021-08-31 20:09:49 +0200

    Filtered api created

---
+++ backend/apiFiltered.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . "/../database/discsDb.php";
+require_once __DIR__ . "/../functions/printJson.php";
+
+// create a variable which contains the genre disc, so I can use it on my function
+$genreType = $_GET["$genre"];
+
+// filter the array discs with array_filter() function => $arrayName, $function($argument), $varToUse
+$filteredDiscsDb = array_filter($discs, function($disc) use($genreType) {
+    return $disc['genre'] === $genreType;
+});
+
+// function to print the filtered array in Json content-type
+printJson($filteredDiscsDb);
+var_dump($filteredDiscsDb);
+
+?>
