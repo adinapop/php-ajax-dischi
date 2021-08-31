eafe65f80530644eba3b3bc517955285255f5dbe
adinapop <adinampop@yahoo.com>
2021-08-31 20:10:12 +0200

    Functions for api and genre list

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 Vue.config.devtools = true; // per estensione
+
 new Vue(    
     {
         el: '#app',
         data: {
             // empty array that will be populated by axios
-            albums: []
+            albums: [],
+            selectGenre: '',
+            filteredAlbums: [],
+            genreList: [],
         },
         created() {
             
@@ -12,6 +16,26 @@ new Vue(
                 .then((result) => {
                     this.albums = result.data;
             })   
+        },
+        methods: {
+            apiFilteredCall() {
+                return "http://localhost:8888/php-ajax-dischi/backend/apiFiltered.php" + "?genre=" + this.selectGenre;
+            },
+
+            getFilteredAlbums() {
+                axios.get(this.apiFilteredCall())
+                    .then((respone) => {
+                        this.filteredAlbums = respone.data
+                })
+            },
+
+            getGenreList() {
+                this.albums.forEach((album) => {
+                  if(!this.genreList.includes(album.genre)){
+                    this.genreList.push(album.genre)
+                  }
+                })
+            },
         }
     }
 );
